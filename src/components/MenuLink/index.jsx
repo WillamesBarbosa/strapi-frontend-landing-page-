63991c359354e06e813b2631ca0eb5e54542c055
Willames <willamessilva1999@gmail.com>
2023-03-20 17:16:51 -0300

    fix: MenuLink fixed up

--- src/components/MenuLink/index.jsx
+++ src/components/MenuLink/index.jsx
@@ -1,7 +1,7 @@
 import P from 'prop-types';
 import * as Styled from './styles';
 
-export const MenuLink = ({ children, link, newTab = true }) => {
+export const MenuLink = ({ children, link, newTab = false }) => {
   const target = newTab ? '_blank' : '_self';
   return (
     <Styled.Container href={link} target={target}>
