4db8ba76b76fd33dd8d729e27654f93e7e4c44f1
Willames <willamessilva1999@gmail.com>
2023-03-21 14:35:11 -0300

    test: LogoLink tested

--- src/components/LogoLink/logoLink.test.jsx
+++ src/components/LogoLink/logoLink.test.jsx
@@ -3,8 +3,20 @@ import { renderTheme } from '../../styles/render-theme';
 import { LogoLink } from '.';
 
 describe('<LogoLink />', () => {
-  it('should render', () => {
-    renderTheme(<LogoLink>Children</LogoLink>);
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+  it('should render text logo', () => {
+    renderTheme(<LogoLink link="#target" text="Algo" />);
+    const heading = screen.getByRole('heading', { name: 'Algo' });
+    expect(heading.firstChild).toHaveAttribute('href', '#target');
+  });
+
+  it('should render image logo', () => {
+    renderTheme(<LogoLink link="#target" text="Algo" src="image.jpg" />);
+    const image = screen.getByAltText('Algo');
+    expect(image).toHaveAttribute('src', 'image.jpg');
+  });
+
+  it('should match snapshot', () => {
+    const { container } = renderTheme(<LogoLink link="#target" text="Algo" src="image.jpg" />);
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
