e5c17011e1010f1d64525c4f3cdf7fccc95b3d0e
Willames <willamessilva1999@gmail.com>
2023-04-11 15:18:31 -0300

    test: GoTop tested

--- src/components/GoTop/GoTop.test.jsx
+++ src/components/GoTop/GoTop.test.jsx
@@ -3,8 +3,18 @@ import { renderTheme } from '../../styles/render-theme';
 import { GoTop } from '.';
 
 describe('<GoTop />', () => {
-  it('should render', () => {
+  it('should render <GoTop />', () => {
     renderTheme(<GoTop>Children</GoTop>);
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'Go to top' })).toBeInTheDocument();
+  });
+
+  it('should find a href', () => {
+    renderTheme(<GoTop>Children</GoTop>);
+    expect(screen.getByRole('link', { name: 'Go to top' })).toHaveAttribute('href', '#');
+  });
+
+  it('should match snapshot', () => {
+    const { container } = renderTheme(<GoTop>Children</GoTop>);
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
